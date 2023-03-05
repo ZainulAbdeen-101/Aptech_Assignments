2322fd478fc08a95f3eeeeac0aa3e6dd73307317
ZainulAbdeen-101 <bbadshah691@gmail.com>
2023-03-05 17:14:38 +0500

    assignment done

---
+++ Assignment 02 (Programming With JavaScript 01)/app.js
@@ -0,0 +1,40 @@
+let celcius=0
+let farhenheit=(celcius-32)*5/9
+
+console.log(`${farhenheit.toFixed(3)} Farhenheit`)
+
+// for celcius to farhenheit
+
+let f=0
+let c=f*1.8 +32
+console.log(`${c} Celcius`)
+
+// for celcius to kelvin
+
+let celcius1=0
+let kelvin=celcius1 +273
+console.log(`${kelvin} kelvin`)
+
+// for K.E Formula
+
+let m=10
+let v=1.3
+
+let KE=(1/2)*m*(v)**2
+console.log(`${KE.toFixed(3)} Joules `)
+
+// for P.E Formula
+
+let m1=10
+let g=9.8
+let h=100
+let PE=m1*g*h
+console.log(`${PE} Joules`)
+
+// for Gravity 
+let M=5.98*10**24
+let G=6.67*10**-11
+let R=6.4*10**6
+
+let gravity=(G*M)/(R)**2
+console.log(gravity)
