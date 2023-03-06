1f6a69547da5f00b0f170e14ac1dda04324d0155
ZainulAbdeen-101 <bbadshah691@gmail.com>
2023-03-06 19:02:33 +0500

    Assignment done

---
+++ Assignment 03 (Two Step Verification)/app.js
@@ -0,0 +1,24 @@
+let email = prompt("plz enter your email");
+
+if (email === "abc@gmail.com") {
+  let pass = Number(prompt("plz enter your password"));
+
+  if (typeof pass == "number") {
+    let entry = true;
+    console.log(pass);
+    if (entry == true) {
+      let NUM = Math.floor(Math.random() * 4 + 100);
+      alert(NUM);
+
+      let match = Number(prompt("Enter the number"));
+
+      if (match === NUM) {
+        console.log("Sign In successful");
+      } else {
+        alert("enter the correct number!");
+      }
+    }
+  }
+} else {
+  alert("enter correct email");
+}
